c79329379fbd726b30deedc86dffae711eecba15
Baozi <baozi.rails@gmail.com>
2014-11-04 14:49:33 +0800

    test

--- app/models/demand.rb
+++ app/models/demand.rb
@@ -4,7 +4,7 @@ class Demand < ActiveRecord::Base
   scope :available, -> { where(instructor_id: nil) }
 
   validates :description, presence: true
-pp
+
   # todo: describe how did I figure this our through recoring casts
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :instructor, class_name: 'User'
